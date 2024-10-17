12646995a9ae6ffee208cb65c07506e9e80e45e0
peppyeben <austineben34@gmail.com>
2024-10-17 16:48:41 +0100

    fix api route

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-                source: "/api/v0/store",
-                // source: "api/v0/:path*",
-                destination: "http://localhost:4000/api/v0/store",
-            },
-            {
-                source: "/api/v0/profile",
-                // source: "api/v0/:path*",
-                destination: "http://localhost:4000/api/v0/profile",
-            },
-            {
                 source: "/api/v0",
                 // source: "api/v0/:path*",
                 destination: "http://localhost:4000/api/v0",
